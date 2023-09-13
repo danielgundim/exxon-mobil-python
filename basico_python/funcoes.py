ffb96791c7fb66308f23def5f7d68450c13de8fc
Daniel Gundim <43392744+danielgundim@users.noreply.github.com>
2023-09-13 19:39:11 -0300

    Aula 4 - Python básico.

---
+++ basico_python/funcoes.py
@@ -0,0 +1,26 @@
+# Funções:
+
+#lista = []
+#
+#
+#def insert_composto(dado):
+#  if type(dado) == type("str"):
+#    lista.append(dado)
+#    return lista
+#  else:
+#    print("Dado não é uma string. Formato inválido.")
+#
+#
+#print(insert("daniel"))
+
+print()
+
+
+def soma_valores(valor1: int, valor2:int) -> int:
+  print(valor1)
+  print(valor2)
+  soma = 4
+  return soma
+
+soma_valores(True,7)
+
---
+++ basico_python/string_1.py
@@ -0,0 +1,10 @@
+nome = "Daniel"
+print(nome)
+print(nome[2])
+
+nome = "Maria"
+sexo = "F"
+idade = 35
+altura = 1.6
+print(f"Nome:  {nome:20} Sexo: {sexo}")
+print(f"Idade: {idade!s:20} Altura: {altura:.3f}")
