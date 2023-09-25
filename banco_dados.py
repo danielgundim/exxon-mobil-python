3deedf68d4b2e8b218b761d899b470927b9caebc
Daniel Gundim <43392744+danielgundim@users.noreply.github.com>
2023-09-25 20:40:31 -0300

    Aula 07 - Banco de dados.

--- banco_dados.py
+++ banco_dados.py
@@ -1,14 +1,11 @@
 import pyodbc
 
-
-def acesso_banco():
-  connection = pyodbc.connect(retorna_conexao_banco_dados())
+def retornar_cursor_banco_dados():
+  connection = pyodbc.connect(retorna_string_conexao_banco_dados())
   cursor = connection.cursor()
-  cursor.execute("select * from cliente;")
-  clientes = cursor.fetchall()
-  print(clientes)
-
-def retorna_conexao_banco_dados():
+  return cursor, connection
+  
+def retorna_string_conexao_banco_dados():
   return(
     "DRIVER={SQL Server};"
     "SERVER=DESKTOP-T62N7HC\SQLEXPRESS;"
@@ -17,4 +14,35 @@ def retorna_conexao_banco_dados():
     "PWD=daniel;"
   )
 
-acesso_banco()
+def select_banco_dados():
+  cursor, connection = retornar_cursor_banco_dados()
+  cursor.execute("select * from cliente;")
+  clientes = cursor.fetchall()
+  print(clientes)
+  connection.commit()
+
+
+def insert_banco_dados(cliente):
+  cursor, connection = retornar_cursor_banco_dados()
+  insert_query = """
+  INSERT INTO cliente (nome, cpf)
+  VALUES (?, ?);
+  """
+
+  values = (cliente['Nome'], cliente['CPF'])
+  cursor.execute(insert_query, values)
+  connection.commit()
+
+
+def delete_banco_dados(cpf):
+  cursor, connection = retornar_cursor_banco_dados()
+  delete_query = "DELETE FROM cliente WHERE cpf = '" + cpf + "';"
+  cursor.execute(delete_query)
+  connection.commit()  
+
+cliente = {"Nome": "Julio", "CPF": "789456123"}
+#insert_banco_dados(cliente)
+
+select_banco_dados()
+delete_banco_dados(cliente["CPF"])
+select_banco_dados()
